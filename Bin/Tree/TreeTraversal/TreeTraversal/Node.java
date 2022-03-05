ab2e7c0f128781b97bda3e8846592ffd17f1f616
PuemMTH <puem.mth@gmail.com>
2022-03-05 13:28:11 +0700

    master

--- Bin/Tree/TreeTraversal/TreeTraversal/Node.java
+++ Bin/Tree/TreeTraversal/TreeTraversal/Node.java
@@ -3,11 +3,9 @@ package TreeTraversal;
 public class Node {
     public String item;
     public Node left, right;
-    
     public Node(String item) {
         this.item = item;
         left = null;
         right = null;
     }
-
 }
